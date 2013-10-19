068b323878ad7f420b3a2853e58f8467e53f4e3c
Neil Mendoza <neil@isthis.gd>
2013-10-19 11:56:46 +0100

    initial commit

---
+++ src/PointCloud.h
@@ -0,0 +1,71 @@
+/*
+ *  PointCloud.h
+ *
+ *  Copyright (c) 2013, Neil Mendoza, http://www.neilmendoza.com
+ *  All rights reserved. 
+ *  
+ *  Redistribution and use in source and binary forms, with or without 
+ *  modification, are permitted provided that the following conditions are met: 
+ *  
+ *  * Redistributions of source code must retain the above copyright notice, 
+ *    this list of conditions and the following disclaimer. 
+ *  * Redistributions in binary form must reproduce the above copyright 
+ *    notice, this list of conditions and the following disclaimer in the 
+ *    documentation and/or other materials provided with the distribution. 
+ *  * Neither the name of Neil Mendoza nor the names of its contributors may be used 
+ *    to endorse or promote products derived from this software without 
+ *    specific prior written permission. 
+ *  
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ *  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+ *  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ *  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ *  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ *  POSSIBILITY OF SUCH DAMAGE. 
+ *
+ */
+#pragma once
+
+namespace itg
+{
+    // This is an exampleof a custom data set class
+    template <typename T>
+    struct PointCloud
+    {
+        std::vector<T> pts;
+        
+        // Must return the number of data points
+        inline size_t kdtree_get_point_count() const { return pts.size(); }
+        
+        // Returns the distance between the vector "p1[0:size-1]" and the data point with index "idx_p2" stored in the class:
+        inline float kdtree_distance(const float *p1, const size_t idx_p2, size_t size) const
+        {
+            const float d0=p1[0]-pts[idx_p2].x;
+            const float d1=p1[1]-pts[idx_p2].y;
+            const float d2=p1[2]-pts[idx_p2].z;
+            return d0*d0+d1*d1+d2*d2;
+        }
+        
+        // Returns the dim'th component of the idx'th point in the class:
+        // Since this is inlined and the "dim" argument is typically an immediate value, the
+        //  "if/else's" are actually solved at compile time.
+        inline float kdtree_get_pt(const size_t idx, int dim) const
+        {
+            if (dim==0) return pts[idx].x;
+            else if (dim==1) return pts[idx].y;
+            else return pts[idx].z;
+        }
+        
+        // Optional bounding-box computation: return false to default to a standard bbox computation loop.
+        //   Return true if the BBOX was already computed by the class and returned in "bb" so it can be avoided to redo it again.
+        //   Look at bb.size() to find out the expected dimensionality (e.g. 2 or 3 for point clouds)
+        template <class BBOX>
+        bool kdtree_get_bbox(BBOX &bb) const { return false; }
+        
+    };
+}
